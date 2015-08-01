72da8b97756bb092eaed4c47d4af5e9e8aab9558
Joy Siriban <joysiriban@gmail.com>
2015-08-01 12:55:17 +0800

    parallax button remove

--- wp/wp-content/themes/Parallax-One/sections/parallax_one_ribbon_section.php
+++ wp/wp-content/themes/Parallax-One/sections/parallax_one_ribbon_section.php
@@ -5,8 +5,8 @@
 
 	$ribbon_background = get_theme_mod('paralax_one_ribbon_background', parallax_get_file('/images/background-images/parallax-img/parallax-img1.jpg'));
 	$parallax_one_ribbon_title = get_theme_mod('parallax_one_ribbon_title','Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-	// $parallax_one_button_text = get_theme_mod('parallax_one_button_text','GET STARTED');
-	// $parallax_one_button_link = get_theme_mod('parallax_one_button_link','#');
+	$parallax_one_button_text = get_theme_mod('parallax_one_button_text','GET STARTED');
+	$parallax_one_button_link = get_theme_mod('parallax_one_button_link','#');
 
 	if(!empty($parallax_one_ribbon_title) || !empty($parallax_one_button_text)){
 
@@ -30,15 +30,15 @@
 								echo '<h2 class="white-text strong paralax_one_only_customizer"></h2>';
 							}
 
-							// if( !empty($parallax_one_button_text) ){
-							// 	if( empty($parallax_one_button_link) ){
-							// 		echo '<button onclick="" class="btn btn-primary standard-button paralax_one_only_customizer" type="button" data-toggle="modal" data-target="#stamp-modal">'.$parallax_one_button_text.'</button>';
-							// 	} else {
-							// 		echo '<button onclick="window.location=\''.esc_url($parallax_one_button_link).'\'" class="btn btn-primary standard-button" type="button" data-toggle="modal" data-target="#stamp-modal">'.esc_attr($parallax_one_button_text).'</button>';
-							// 	}
-							// } elseif ( isset( $wp_customize )   ) {
-							// 	echo '<button class="btn btn-primary standard-button paralax_one_only_customizer" type="button" data-toggle="modal" data-target="#stamp-modal"></button>';
-							// }
+							if( !empty($parallax_one_button_text) ){
+								if( empty($parallax_one_button_link) ){
+									echo '<button onclick="" class="btn btn-primary standard-button paralax_one_only_customizer" type="button" data-toggle="modal" data-target="#stamp-modal">'.$parallax_one_button_text.'</button>';
+								} else {
+									echo '<button onclick="window.location=\''.esc_url($parallax_one_button_link).'\'" class="btn btn-primary standard-button" type="button" data-toggle="modal" data-target="#stamp-modal">'.esc_attr($parallax_one_button_text).'</button>';
+								}
+							} elseif ( isset( $wp_customize )   ) {
+								echo '<button class="btn btn-primary standard-button paralax_one_only_customizer" type="button" data-toggle="modal" data-target="#stamp-modal"></button>';
+							}
 						?>
 
 					</div>
